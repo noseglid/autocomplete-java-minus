1fea3aeb07146fde8cc22ab5779fa4b233842d68
Alexander Olsson <noseglid@gmail.com>
2016-05-23 13:42:50 +0200

    Refactor parseDotChain
    Moved to util

--- spec/util-spec.js
+++ spec/util-spec.js
@@ -1,54 +1,48 @@
 'use babel';
 
-import { DotEntryFinder } from '../lib/entryfinders';
+import { parseDotChain } from '../lib/util';
 
-describe('DotEntryFinder', () => {
-  let finder;
-
-  beforeEach(() => {
-    finder = new DotEntryFinder(null);
-  });
-
+describe('Util', () => {
   describe('parseDotChain', () => {
     it('should split on dots between static classes', () => {
-      expect(finder.parseDotChain('Root.Class.NestedClass'))
+      expect(parseDotChain('Root.Class.NestedClass'))
         .toEqual([ 'Root', 'Class', 'NestedClass' ]);
     });
 
     it('should parse an empty string if code ends with a dot', () => {
-      expect(finder.parseDotChain('Root.'))
+      expect(parseDotChain('Root.'))
         .toEqual([ 'Root', '' ]);
     });
 
     it('should work with no-args method calls', () => {
-      expect(finder.parseDotChain('Root.method().Class'))
+      expect(parseDotChain('Root.method().Class'))
         .toEqual([ 'Root', 'method()', 'Class' ]);
     });
 
     it('should work with arguments in method calls', () => {
-      expect(finder.parseDotChain('Root.method(someArgument).Class'))
+      expect(parseDotChain('Root.method(someArgument).Class'))
         .toEqual([ 'Root', 'method(someArgument)', 'Class' ]);
 
-      expect(finder.parseDotChain('Root.method(arg1, arg2).Class'))
+      expect(parseDotChain('Root.method(arg1, arg2).Class'))
         .toEqual([ 'Root', 'method(arg1, arg2)', 'Class' ]);
     });
 
     it('should work with quoted arguments', () => {
-      expect(finder.parseDotChain('Root.method("this arg").Class'))
+      expect(parseDotChain('Root.method("this arg").Class'))
         .toEqual([ 'Root', 'method("this arg")', 'Class' ]);
 
-      expect(finder.parseDotChain('Root.method("this.arg").Class'))
+      expect(parseDotChain('Root.method("this.arg").Class'))
         .toEqual([ 'Root', 'method("this.arg")', 'Class' ]);
 
-      expect(finder.parseDotChain('Root.method("this.arg", Some.Dotted.Path).Class'))
+      expect(parseDotChain('Root.method("this.arg", Some.Dotted.Path).Class'))
         .toEqual([ 'Root', 'method("this.arg", Some.Dotted.Path)', 'Class' ]);
 
-      expect(finder.parseDotChain('Root.method("quoted\\"escaped(").Class'))
+      expect(parseDotChain('Root.method("quoted\\"escaped(").Class'))
         .toEqual([ 'Root', 'method("quoted\\"escaped(")', 'Class' ]);
     });
 
     it('should work with lambdas', () => {
-      expect(finder.parseDotChain('Root.method(someArg -> someArg.doStuff())'))
+      expect(parseDotChain('Root.method(someArg -> someArg.doStuff())'))
         .toEqual([ 'Root', 'method(someArg -> someArg.doStuff())' ]);
     });
   });
