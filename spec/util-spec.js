41f68611a0265b71105025e47723e169cc0ca1c1
Alexander Olsson <noseglid@gmail.com>
2016-05-25 16:44:27 +0200

    Extract dotchain properly.
    More involed than one may think. Gotta keep track of context so
    regexes are not really possible (or at least very difficult).
    Includes specs.

--- spec/util-spec.js
+++ spec/util-spec.js
@@ -1,8 +1,75 @@
 'use babel';
 
-import { parseDotChain } from '../lib/util';
+import { extractDotChain, parseDotChain } from '../lib/util';
 
 describe('Util', () => {
+  describe('extractDotChain', () => {
+    it('should extract plain variables when there are spaces to previous token', () => {
+      expect(extractDotChain('return variable'))
+        .toEqual('variable');
+    });
+
+    it('should extract members of plain variables when there are spaces to previous token', () => {
+      expect(extractDotChain('return variable.method'))
+        .toEqual('variable.method');
+    });
+
+    it('should extract plain method calls when there are spaces to previous token', () => {
+      expect(extractDotChain('return someMethod()'))
+        .toEqual('someMethod()');
+    });
+
+    it('should extract member method calls when there are spaces to previous token', () => {
+      expect(extractDotChain('return variable.method()'))
+        .toEqual('variable.method()');
+    });
+
+    it('should extract methods with quoted arguments', () => {
+      expect(extractDotChain('return variable.method("string")'))
+        .toEqual('variable.method("string")');
+    });
+
+    it('should extract methods with dotted arguments', () => {
+      expect(extractDotChain('return variable.method(Domain.Namespace.Class.method())'))
+        .toEqual('variable.method(Domain.Namespace.Class.method())');
+    });
+
+    it('should extract when there are newlines', () => {
+      expect(extractDotChain('return\nvariable\n.method("string",\nAnother.arg.methodCall())'))
+        .toEqual('variable.method("string",Another.arg.methodCall())');
+    });
+
+    it('should extract with multiple whitespaces combined', () => {
+      expect(extractDotChain('variable\n.argument.\nother\n\n\n\n       . deep'))
+        .toEqual('variable.argument.other.deep');
+    });
+
+    it('should extract when there\'s very compact code', () => {
+      expect(extractDotChain('int a=5;variable.methodCall().otherThing'))
+        .toEqual('variable.methodCall().otherThing');
+    });
+
+    it('should extract the first statement of a method', () => {
+      expect(extractDotChain('public void m(Root.Class somearg) {\n return someVariable.result()'))
+        .toEqual('someVariable.result()');
+    });
+
+    it('should extract the first statement of a method even if very compact code', () => {
+      expect(extractDotChain('public void method(){somevariable.method()'))
+        .toEqual('somevariable.method()');
+    });
+
+    it('should extract nested completions', () => {
+      expect(extractDotChain('someMethodCall(Root.completion.other("args")'))
+        .toEqual('Root.completion.other("args")');
+    });
+
+    it('should ignore escaped quotes in quotes', () => {
+      expect(extractDotChain('var.someMethodCall(" \\"quote me\\" ")'))
+        .toEqual('var.someMethodCall(" \\"quote me\\" ")');
+    });
+  });
+
   describe('parseDotChain', () => {
     it('should split on dots between static classes', () => {
       expect(parseDotChain('Root.Class.NestedClass'))
